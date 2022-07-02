e0072ee582e9a914d6a6a73cffe41d0f0a74aa86
Ivan Tur <gajtep@gmail.com>
2022-07-02 22:40:12 +0300

    2 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
-const numbCategories = document.querySelectorAll('.item');
+const numbCategories = document.querySelectorAll(".item");
 
 console.log(`Number of categories ${numbCategories.length}`);
 
 numbCategories.forEach((element) => {
-    return console.log(`Category: ${element.querySelector("h2").textContent}
+  return console.log(`Category: ${element.querySelector("h2").textContent}
     Element: ${element.querySelectorAll("li").length}`);
-}
-
-)
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingridientList = document.querySelector("#ingredients");
+const addItem = ingredients.map((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+  return li;
+});
+ingridientList.append(...addItem);
